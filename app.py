3032c6ba32a85454b99700d412a6d4852ade2a7d
Shrijan <shrijankarki96@gmail.com>
2020-12-13 23:07:51 -0500

    Seventh Commit

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pymongo
 
 app = Flask(__name__, template_folder='Templates')
 
+client = pymongo.MongoClient(
+    "mongodb+srv://ShrijanK:dummyPassword1234@cluster0.wfpuf.mongodb.net/DprogDb?retryWrites=true&w=majority")
+myDb = client["DprogDb"]
 
 @app.route('/')
 def hello():
@@ -11,12 +14,30 @@ def hello():
 
 @app.route('/google-charts')
 def google_pie_chart():
-    client = pymongo.MongoClient(
-    "mongodb+srv://ShrijanK:dummyPassword1234@cluster0.wfpuf.mongodb.net/DprogDb?retryWrites=true&w=majority")
-    myDb = client["DprogDb"]
     data = {"Provinces": "Total Cases"}
     for coll in myDb.list_collection_names():
         inner_dict = myDb[coll].find_one({}, {"_id": 0, "Date": 0})
         data[coll] = int((inner_dict["Total Cases"].replace(',', '')))  # Returns Total Number of Cases Only
     print(data)
     return render_template('pie-chart.html', data=data)
+
+
+@app.route('/line-chart')
+def google_line_chart():
+    data = []
+    val = myDb["Ontario"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/api/all', methods=['GET'])
+def api_all():
+    data = []
+    for coll in myDb.list_collection_names():
+        tb_data = myDb[coll].find({}, {"_id": 0})
+        for txt in tb_data:
+            data.append(txt)
+    return jsonify(data)
