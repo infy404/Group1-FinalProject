7e24e8e983805b47bd8c8857ed1a1982015307e2
Shrijan <shrijankarki96@gmail.com>
2020-12-18 21:13:34 -0500

    Eigth Commit

--- app.py
+++ app.py
@@ -8,11 +8,6 @@ client = pymongo.MongoClient(
 myDb = client["DprogDb"]
 
 @app.route('/')
-def hello():
-    return render_template('homeFile.html')
-
-
-@app.route('/google-charts')
 def google_pie_chart():
     data = {"Provinces": "Total Cases"}
     for coll in myDb.list_collection_names():
@@ -22,10 +17,142 @@ def google_pie_chart():
     return render_template('pie-chart.html', data=data)
 
 
-@app.route('/line-chart')
-def google_line_chart():
+@app.route('/Ontario')
+def line_chart_ON():
     data = []
     val = myDb["Ontario"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/British-Columbia')
+def line_chart_BC():
+    data = []
+    val = myDb["British Columbia"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Alberta')
+def google_line_chart():
+    data = []
+    val = myDb["Alberta"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Saskatchewan')
+def line_chart_SK():
+    data = []
+    val = myDb["Saskatchewan"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Manitoba')
+def line_chart_MN():
+    data = []
+    val = myDb["Manitoba"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Quebec')
+def line_chart_QB():
+    data = []
+    val = myDb["Quebec"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/New-Brunswick')
+def line_chart_NB():
+    data = []
+    val = myDb["New Brunswick"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Nova-Scotia')
+def line_chart_NS():
+    data = []
+    val = myDb["Nova Scotia"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Prince-Edward-Island')
+def line_chart_PEI():
+    data = []
+    val = myDb["Prince Edward Island"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Newfoundland-and-Labrador')
+def line_chart_NaL():
+    data = []
+    val = myDb["Newfoundland and Labrador"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Yukon')
+def line_chart_YK():
+    data = []
+    val = myDb["Yukon"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Northwest-Territories')
+def line_chart_NWT():
+    data = []
+    val = myDb["Northwest Territories"].find({}, {"_id": 0})
+    for txt in val:
+        pair = [v for k, v in txt.items()]
+        data.append([pair[0], int(pair[1].replace(',', ''))])
+
+    return render_template('line-chart.html', data=data)
+
+
+@app.route('/Nunavut')
+def line_chart_NUT():
+    data = []
+    val = myDb["Nunavut"].find({}, {"_id": 0})
     for txt in val:
         pair = [v for k, v in txt.items()]
         data.append([pair[0], int(pair[1].replace(',', ''))])
